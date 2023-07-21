04459f544246d06e44bb56e01f29fae4359331c1
Emirhan Yıldız <emirrhanyildiz@gmail.com>
2023-07-21 16:57:34 +0300

    Update FleuryAlgorithm.java

--- fleury/FleuryAlgorithm.java
+++ fleury/FleuryAlgorithm.java
@@ -1,87 +1,104 @@
 import java.util.*;
 
 public class FleuryAlgorithm {
-    private int vertices;
-    private List<List<Integer>> adjList;
-    
-    public FleuryAlgorithm(int v) {
-        vertices = v;
-        adjList = new ArrayList<>(v);
-        for (int i = 0; i < v; i++) {
-            adjList.add(new LinkedList<>());
-        }
-    }
-    
-    public void addEdge(int u, int v) {
-        adjList.get(u).add(v);
-        adjList.get(v).add(u);
-    }
-    
-    private boolean isValidNextEdge(int u, int v) {
-        // Count the number of remaining edges
-        int count = 0;
-        for (int i : adjList.get(u)) {
-            if (i != -1) {
-                count++;
-            }
-        }
+ 
+    public static void main(String[] args) {
+        Graph graph = new Graph();
         
-        // If there's only one remaining edge, it's a valid next edge
-        if (count == 1) {
+        graph.addEdge("A","B");
+        graph.addEdge("A","C");
+        graph.addEdge("B","C");
+        graph.addEdge("C","D");
+        graph.addEdge("D","E");
+        graph.addEdge("D","F");
+        graph.addEdge("D","G");
+        graph.addEdge("E","F");
+        graph.addEdge("F","G");
+        
+        System.out.print("Euler Path: ");
+        graph.printEulerPath();
+    }
+}
+
+
+class Graph{
+	private Map<String, List<String>> adjVertices= new HashMap<>();
+
+    public void addEdge(String src, String dest) {
+        if(!adjVertices.containsKey(src)){
+            addVertex(src);
+        }
+        if(!adjVertices.containsKey(dest)){
+            addVertex(dest);
+        }
+        adjVertices.get(src).add(dest);
+        adjVertices.get(dest).add(src);
+    }
+
+    private void removeEdge(String u, String v) {
+        adjVertices.get(u).remove(v);
+        adjVertices.get(v).remove(u);
+    }
+
+    public void addVertex(String vertex) {
+        adjVertices.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public Map<String, List<String>> getAdj() {
+		return adjVertices;
+	}
+
+    private boolean isValidNextEdge(String u, String v) {
+        
+        if (adjVertices.get(u).size() == 1) {
             return true;
         }
         
-        // Check if removing the edge u-v disconnects the graph
-        boolean[] visited = new boolean[vertices];
-        int count1 = DFSCount(u, visited);
+        int count1 = DFS(u).size();
         
-        // Remove the edge u-v and check if it disconnects the graph
         removeEdge(u, v);
-        Arrays.fill(visited, false);
-        int count2 = DFSCount(u, visited);
+
+        int count2 = DFS(u).size();
         
-        // Add the edge back to the graph
         addEdge(u, v);
         
         return (count1 > count2) ? false : true;
     }
-    
-    private int DFSCount(int v, boolean[] visited) {
-        visited[v] = true;
-        int count = 1;
-        
-        for (int i : adjList.get(v)) {
-            if (i != -1 && !visited[i]) {
-                count += DFSCount(i, visited);
+
+    public List<String> DFS(String node){
+        List<String> result = new ArrayList<>();
+        DFSUtil(node,result);
+        return result;
+    }
+    public void DFSUtil(String node,List<String> result){
+        if(!result.contains(node)){
+            result.add(node);
+            for (String n : adjVertices.get(node)) {
+                DFSUtil(n,result);
             }
         }
-        
-        return count;
     }
-    
-    private void removeEdge(int u, int v) {
-        adjList.get(u).remove(new Integer(v));
-        adjList.get(v).remove(new Integer(u));
-    }
-    
+
     public void printEulerPath() {
-        int u = 0; // Starting vertex for Euler's path
-        
-        // Find a vertex with an odd degree as the starting vertex
-        for (int i = 0; i < vertices; i++) {
-            if (adjList.get(i).size() % 2 != 0) {
+        String u = null;
+        Iterator <String> iterator = adjVertices.keySet().iterator();
+        if (iterator.hasNext()) {
+            u = (String) iterator.next();
+        }        
+
+        for (String i : adjVertices.keySet()) {
+            if (adjVertices.get(i).size() % 2 != 0) {
                 u = i;
                 break;
             }
         }
-        
         printEulerUtil(u);
         System.out.println();
     }
-    
-    private void printEulerUtil(int u) {
-        for (int v : adjList.get(u)) {
-            if (v != -1 && isValidNextEdge(u, v)) {
+
+    private void printEulerUtil(String u) {
+        for (String v : adjVertices.get(u)) {
+            if (isValidNextEdge(u, v)) {
                 System.out.print(u + "-" + v + " ");
                 removeEdge(u, v);
                 printEulerUtil(v);
@@ -89,18 +106,5 @@ public class FleuryAlgorithm {
             }
         }
     }
-    
-    public static void main(String[] args) {
-        FleuryAlgorithm graph = new FleuryAlgorithm(4);
-        
-        // Add edges
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 3);
-        
-        // Print Euler's path
-        System.out.print("Euler Path: ");
-        graph.printEulerPath();
-    }
+
 }
